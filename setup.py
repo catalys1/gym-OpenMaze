19bfc46da5973c49d537d260bb481d0b9e8e3c0a
Connor <thecatalystak@gmail.com>
2018-03-05 16:02:00 -0700

    Added a missin comma to setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
 	name='gym_openmaze',
 	version='0.0.1',
-	install_requires=['gym','numpy']
+	install_requires=['gym','numpy'],
 	packages=['gym_openmaze','gym_openmaze.envs']
 )
 
