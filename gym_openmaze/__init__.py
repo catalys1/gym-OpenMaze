c976411862f95f4d6f1376ad9b8575ba9d892161
Connor <thecatalystak@gmail.com>
2018-03-05 17:25:57 -0700

    Added max_episode_steps to __init__

--- gym_openmaze/__init__.py
+++ gym_openmaze/__init__.py
@@ -2,5 +2,6 @@ from gym.envs.registration import register
 
 register(
 	id='openmaze-v0',
-	entry_point='gym_openmaze.envs:OpenMaze'
+	entry_point='gym_openmaze.envs:OpenMaze',
+	max_episode_steps=1000
 )
